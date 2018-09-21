adc962d6ec4c1b44da6f716542e947c088e2b3aa
Tanya Kasa <kasatanya1@gmail.com>
2018-09-21 13:20:49 +0100

    updated website

--- api/router.js
+++ api/router.js
@@ -2,13 +2,52 @@
  * Created by tkasa on 15/06/2018.
  */
 
-var express = require('express');
-var router = require('express').Router();
-router.get('/', function (req,res) {
-    res.render('index',{
-        siteTitle: 'Alpha Renovation',
-        pageTitle: 'Home page'
-    })
-});
+const express = require('express');
+const router = express.Router();
+const flash = require('connect-flash');
 
-module.exports = router;
+module.exports = function() { //app was before
+
+    router.use('/', function (req, res) {
+        res.render('index', {
+            siteTitle: 'Alpha Renovation',
+            pageTitle: 'Home page'
+        })
+    });
+
+    router.use('/index', function (req, res) {
+        res.render('index',{
+            pageTitle: 'Alpha Renovation Company'
+        });
+    });
+
+    router.use('/about', function (req, res) {
+        res.render('about',{
+            pageTitle: 'About Us'
+        });
+    });
+
+    router.use('/services', function (req, res) {
+        res.render('services',{
+            pageTitle: 'Our Services'
+        });
+    });
+
+    router.get('/contact', function (req, res) {
+        res.render('contact',{
+            pageTitle: 'Please get in contact with us.'
+        });
+    });
+
+    router.get('/contact_send', function (req, res) {
+        res.render('contact-send',{
+            pageTitle: 'Thank you for your detalies.'
+        });
+    });
+
+    router.get('/portfolio', function (req, res) {
+        res.render('portfolio',{
+            pageTitle: 'Our Portfolio'
+        })
+    });
+};
